0611ac2a7fe992d863ecaaa950ebd31cc63e3ef6
Mark Wolfe <mark.wolfe@buildkite.com>
2024-10-07 12:49:12 +1100

    fix: update the output path for compile time file

--- build-kernel.sh
+++ build-kernel.sh
@@ -2,11 +2,13 @@
 
 set -e -o pipefail
 
+WORKDIR=$(dirname "$0")
+
 echo "-- build kernel"
 cd linux-6.8
 make defconfig
 
-time ( make -j"$(nproc)" 2>&3 ) 3>&2 2>compile_time_output.txt
+time ( make -j"$(nproc)" 2>&3 ) 3>&2 2>"${WORKDIR}/compile_time_output.txt"
 
 echo "-- clean up"
 make clean
